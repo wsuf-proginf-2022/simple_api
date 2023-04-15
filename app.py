1dac18a8e4f1157a11ad7459be96f9b9cc93cbb8
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-04-15 16:34:17 +0200

    epic

--- app.py
+++ app.py
@@ -1,8 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+projects = [{
+    'name': 'my first project',
+    'tasks': [{
+        'name': 'my first task',
+        'completed': False
+    }]
+}]
+
 
 @app.route("/")
-def hello_world():
-  return render_template("index.html")
+def home():
+  return render_template("index.html.j2", name="Zsolt")
+
+
+@app.route("/projects")
+def get_projects():
+  return jsonify({'projects': projects})
+
+
+@app.route("/project/<string:name>")
+def get_project(name):
+  print(name)
+  for project in projects:
+    if project['name'] == name:
+      return jsonify(project)
+  return jsonify({'message': 'project not found'}), 404
+
+
+@app.route("/project/<string:name>/tasks")
+def get_project_tasks(name):
+  for project in projects:
+    if project['name'] == name:
+      return jsonify({'tasks': project['tasks']})
+  return jsonify({'message': 'project not found'}), 404
+
+
+@app.route("/project/<string:name>/task", methods=['POST'])
+def add_task_to_project(name):
+  request_data = request.get_json()
+  for project in projects:
+    if 'name' in project and project['name'] == name:
+      if 'completed' not in request_data or type(
+          request_data['completed']) is not bool:
+        return jsonify(
+            {'message': 'completed is required and must be a boolean'}), 400
+      new_task = {
+          'name': request_data['name'],
+          'completed': request_data['completed']
+      }
+      project['tasks'].append(new_task)
+      return jsonify(new_task)
+  return jsonify({'message': 'project not found'}), 404
